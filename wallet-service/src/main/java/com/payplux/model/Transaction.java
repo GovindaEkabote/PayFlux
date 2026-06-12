cdbc002b52452195c66d109bce9202499c7de432
GovindaEkbote <govindaekbote7@gmail.com>
2026-06-12 23:44:59 +0530

    create Repository interfaces

---
+++ wallet-service/src/main/java/com/payplux/model/Transaction.java
@@ -0,0 +1,67 @@
+package com.payplux.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+@Data
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String referenceId;
+
+    @Column(nullable = false)
+    private Long senderId;
+
+    @Column(nullable = false)
+    private Long receiverId;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    private String remarks;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+
+        if (referenceId == null) {
+            referenceId = UUID.randomUUID().toString();
+        }
+
+        if (status == null) {
+            status = TransactionStatus.PENDING;
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ wallet-service/src/main/java/com/payplux/repository/WalletHoldRepository.java
@@ -0,0 +1,18 @@
+package com.payplux.repository;
+
+import com.payplux.model.WalletHold;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WalletHoldRepository extends JpaRepository<WalletHold, Long> {
+
+    Optional<WalletHold> findByHoldReference(String holdReference);
+
+    List<WalletHold>  findByStatusAndExpireAtBefore(String active, LocalDateTime now);
+
+}
